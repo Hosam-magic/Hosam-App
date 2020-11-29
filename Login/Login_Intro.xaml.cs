90db47b42e16d5754b24c96c72b5099cbe33a4e2
ian's desktop\ian <a52j16@gmail.com>
2020-11-29 16:43:42 +0800

    原始版本-1129

---
+++ Login/Login_Intro.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hosam_App
+{
+    /// <summary>
+    /// Login_Intro.xaml 的互動邏輯
+    /// </summary>
+    public partial class Login_Intro : Page
+    {
+        private const string V = "Login/Login_Load.xaml";
+        MainWindow _mainWindow =null;
+        public Login_Intro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _mainWindow = Window.GetWindow(this) as MainWindow;
+
+
+            this._mainWindow.Main.Navigate(new Uri(V, UriKind.Relative));
+        }
+    }
+}
---
+++ Login/Login_Load.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace Hosam_App
+{
+    /// <summary>
+    /// Login_Load.xaml 的互動邏輯
+    /// </summary>
+    public partial class Login_Load : Page
+    {
+        MainWindow _mainWindow = null;
+        public Login_Load()
+        {
+            InitializeComponent();
+        }IEnumerator Cunting() {
+            
+             _mainWindow = Window.GetWindow(this) as MainWindow;
+            _mainWindow.Width = 1200;
+            this._mainWindow.Main.Navigate(new Uri("", UriKind.Relative));
+            yield return null;
+        }
+    }
+}

cddf33faedd8346bb466d1e3de432fc51bb2186e
twbin <a8343656@gmail.com>
2020-12-05 12:46:56 +0800

    測試引入遊戲路徑 (尚未完成)

---
+++ DTO/GameDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hosam_App.DTO
+{
+    class GameDetail
+    {
+        string viewName;
+        string gameName;
+        string path;
+        string driver;
+    }
+}

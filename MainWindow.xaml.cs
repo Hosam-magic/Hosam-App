c1610872300e137a3de8b826375398ddb47ff563
twbin <a8343656@gmail.com>
2021-12-02 15:49:38 +0800

    更改為簡潔的寫法

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,10 +13,16 @@ namespace Hosam_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        Frame frame;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            MainWindow mainWindow = GetWindow(this) as MainWindow;
+            // 用xname 取得frame :
+            frame = (Frame)mainWindow.FindName("test");
+
             MinimizeButton.Click += (s, e) => WindowState = WindowState.Minimized;
             MaxmizeButton.Click += (s, e) => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
             CloseButton.Click += (s, e) => Close();
@@ -26,32 +32,18 @@ namespace Hosam_App
 
         public void Window_Loaded(Object sender ,RoutedEventArgs e) 
         {
-            
-
-            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
-
-            // 用xname 取得frame :
-            Frame frame = (Frame)mainWindow.FindName("test");
             // frame 用  Navigate
             frame.Navigate(new HomePage());
         }
 
         private void HomeButton_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
-
-            // 用xname 取得frame :
-            Frame frame = (Frame)mainWindow.FindName("test");
             // frame 用  Navigate
             frame.Navigate(new HomePage());
         }
 
         private void GameButton_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
-
-            // 用xname 取得frame :
-            Frame frame = (Frame)mainWindow.FindName("test");
             // frame 用  Navigate
             frame.Navigate(new SideBar());
         }
